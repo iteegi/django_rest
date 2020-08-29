accd02a74450ae5503f1555de72c19d8ae91543f
J D <jd@gmail.com>
2020-08-30 01:56:06 +0300

    created PostSerializer class

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,1 +1,19 @@
 """Serializers for Posts models."""
+
+from rest_framework import serializers
+from .post import Posts
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """Serializer for posts models."""
+
+    class Meta:
+        """Additional settings for the PostSerializer class."""
+
+        model = Posts
+        fields = ['id',
+                  'title',
+                  'url',
+                  'poster',
+                  'created',
+                  ]
