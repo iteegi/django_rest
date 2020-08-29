84bd9c6cfe7c8e14e2f23f371d5d5f1a8d8ed002
J D <jd@gmail.com>
2020-08-30 01:19:21 +0300

    added docstring in Post model

--- posts/models.py
+++ posts/models.py
@@ -3,10 +3,14 @@ from django.contrib.auth.models import User
 
 
 class Posts(models.Model):
+    """Posts model."""
+
     title = models.CharField(max_length=100)
     url = models.URLField()
     poster = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        """Additional settings for the Posts model."""
+
         orderin = ['-created']
