a6cf4736b7b2e6e4bf3a1b3fd8d530c69b232c13
J D <jd@gmail.com>
2020-09-07 02:02:55 +0300

    created VoteCreate

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@
 from rest_framework import generics, permissions
 
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, VoteSerializer
 
 
 class PostList(generics.ListCreateAPIView):
@@ -19,3 +19,11 @@ class PostList(generics.ListCreateAPIView):
         Called by CreateModelMixin.
         """
         serializer.save(poster=self.request.user)
+
+
+class VoteCreate(generics.CreateAPIView):
+    """View to vote for a specific post."""
+
+    queryset = Post.objects.all()
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
